3dd8205cf615c1fc76d42f77737290d6c8303e28
shoaib-mohiuddin <shoaib.mohiuddin@cloudreach.com>
2022-08-22 15:00:22 +0530

    Updates required variables

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "owner" {
+  description = "Owner of the AMI"
+  type        = string
+}
+
+variable "ami_elasticsearch" {
+  description = "AMI for Elasticsearch"
+  type        = string
+}
+
+variable "ami_logstash" {
+  description = "AMI for Logstash"
+  type        = string
+}
+
+variable "ami_kibana" {
+  description = "AMI for Kibana"
+  type        = string
+}
+
+variable "ami_filebeat" {
+  description = "AMI for Filebeat"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type"
+  type        = string
+}
