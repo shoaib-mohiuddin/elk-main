4a45510f06597556acb317d1407810c86d717dc8
shoaib-mohiuddin <shoaib.mohiuddin@cloudreach.com>
2022-08-23 13:16:10 +0530

    multi-az deploy

--- demo_servers.tf
+++ demo_servers.tf
@@ -24,25 +24,25 @@ resource "aws_instance" "demo_server_1" {
     Name = "Demo-1"
   }
 }
-# resource "aws_instance" "demo_server_2" {
-#   #ami           = data.aws_ami.ubuntu.id
-#   instance_type = "t3.medium"
-#   #vpc_security_group_ids = [aws_security_group.ansible_server.id]
-#   #key_name = "talent-academy-lab"
-#   subnet_id = data.aws_subnet.app_private_b.id
+resource "aws_instance" "demo_server_2" {
+  ami           = data.aws_ami.ami_filebeat.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
+  key_name = "ta-lab"
+  subnet_id = data.aws_subnet.app_private_b.id
 
-#   tags = {
-#     Name = "Demo-2"
-#   }
-# }
-# resource "aws_instance" "demo_server_3" {
-#   #ami           = data.aws_ami.ubuntu.id
-#   instance_type = "t3.medium"
-#   #vpc_security_group_ids = [aws_security_group.ansible_server.id]
-#   #key_name = "talent-academy-lab"
-#   subnet_id = data.aws_subnet.app_private_c.id
+  tags = {
+    Name = "Demo-2"
+  }
+}
+resource "aws_instance" "demo_server_3" {
+  ami           = data.aws_ami.ami_filebeat.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
+  key_name = "ta-lab"
+  subnet_id = data.aws_subnet.app_private_c.id
 
-#   tags = {
-#     Name = "Demo-3"
-#   }
-# }
+  tags = {
+    Name = "Demo-3"
+  }
+}
